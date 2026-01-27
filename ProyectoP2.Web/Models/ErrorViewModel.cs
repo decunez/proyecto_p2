5517496c1c7cebc5ef6eca81272de76e51d71c26
decunez <decunez@espe.edu.ec>
2026-01-26 22:23:46 -0500

    update

--- ProyectoP2.Web/Models/ErrorViewModel.cs
+++ ProyectoP2.Web/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace ProyectoP2.Web.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
